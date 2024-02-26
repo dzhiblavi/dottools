80ad9c06e0dd50b182abd99abd6a2f3b6e054ee4
Ibragim Dzhiblavi <dzhiblavi@yandex-team.ru>
2024-02-26 18:58:20 +0300

    update

--- dt/config/builder.py
+++ dt/config/builder.py
@@ -1,12 +1,11 @@
 from typing import Any, Dict, List, Optional
-from functools import reduce
+from functools import reduce, partial
 
 from dt.util import merge
 from dt.config.config import Config, LIST_META_KEY, FROM_META_KEY
 
 
 def _apply_meta_kv(key, value):
-    # if kv-pair is <from, value> && if value is not a list, make it a list
     if key == FROM_META_KEY and not isinstance(value, list):
         value = [value]
 
@@ -35,43 +34,35 @@ def _get_objects_to_merge_to_and_remove_from_key(
         from_objs = from_objs[LIST_META_KEY]
 
     assert isinstance(from_objs, list)
-    return list(map(lambda from_obj: _apply_merging(from_obj, opts), from_objs))
+    return [_apply_merging(item, opts) for item in from_objs]
 
 
 def _apply_merging_impl(obj: Any, opts: Dict[str, str]) -> Any:
     if FROM_META_KEY not in obj:
         return obj
 
-    # Merge all objects in 'from' list sequentially
     merged_from_objects = reduce(
-        lambda val, from_obj: merge.merge(val, from_obj, opts),
+        partial(merge.merge, opts=opts),
         _get_objects_to_merge_to_and_remove_from_key(obj, opts),
     )
 
-    # Finally, merge obj (without 'from' key) into merged base objects
     return merge.merge(merged_from_objects, obj, opts)
 
 
 def _apply_merging(obj: Any, opts: Dict[str, str]) -> Any:
     if isinstance(obj, dict):
-        # Get merge options from obj and merge them into
-        # parent's merge options opts
         opts = merge.get_merge_opts(obj, opts)
 
-        # first, apply merging to all subobjects
         for key, value in obj.items():
             obj[key] = _apply_merging(value, opts)
 
-        # now we can merge the obj
         obj = _apply_merging_impl(obj, opts)
 
-        # remove utility key from the object
         if merge.MERGE_OPTS_CONFIG_KEY in obj:
             del obj[merge.MERGE_OPTS_CONFIG_KEY]
 
     if isinstance(obj, list):
-        # just apply merging to all elements in the list
-        return list(map(lambda from_obj: _apply_merging(from_obj, opts), obj))
+        return [_apply_merging(item, opts) for item in obj]
 
     return obj
 
