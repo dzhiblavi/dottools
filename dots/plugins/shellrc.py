ab805e2619667725a172bed7bfaa7b17959dc730
Ibragim Dzhiblavi <dzhiblavi@yandex-team.ru>
2024-08-29 11:22:44 +0300

    add minimal & fix empty paths

--- dots/plugins/shellrc.py
+++ dots/plugins/shellrc.py
@@ -8,23 +8,6 @@ from dots.context import context
 from dots.plugins import plugin, file
 
 
-def _create_prompt(
-    prompt_style: List[str], local: Optional[Dict[str, Any]] = None
-) -> str:
-    prompt = ""
-
-    for part in prompt_style:
-        prompt += context().apply(str(part), local)
-
-    return prompt
-
-
-def _get_prompt(config: Config) -> str:
-    return _create_prompt(
-        prompt_style=config.get("prompt-style", []).astype(list),
-    )
-
-
 def _write_scripts(config: Config, kind: str, out: List[str]) -> None:
     for script_path in config.get(kind, []).astype(list):
         with open(script_path.astype(str), "r", encoding="utf-8") as script_f:
@@ -54,31 +37,26 @@ def _write_assignment(
 
 
 def _write_path(config: Config, out: List[str]) -> None:
+    paths = config.get("path", []).astype(list)
+
+    if not paths:
+        return
+
     path_env = ""
-    for entry in config.get("path", []).astype(list):
+    for entry in paths:
         path_env += ":" + entry.astype(str)
+
     out.append(f"export PATH={path_env}:${{PATH}}\n")
 
 
-def _write_prompt(config: Config, out: List[str]) -> None:
-    if config.get("disable_prompt_env", False).astype(bool):
-        return
-
-    env_dict = {
-        env.PROMPT_ENV_VAR: f"'{_get_prompt(config)}'",
-    }
-
-    for k, value in env_dict.items():
-        out.append(f"export {k}={value}\n")
-    out.append("\n")
-
-
 def _create_shellrc(config: Config) -> List[str]:
+    minimal = config.getp("minimal", False).astype(bool)
+
     out: List[str] = []
     _write_scripts(config, "pre", out)
-    _write_base_env(config, out)
+    if not minimal:
+        _write_base_env(config, out)
     _write_path(config, out)
-    _write_prompt(config, out)
     _write_scripts(config, "mid", out)
     _write_assignment(config, "export", "env", out, wrap='"')
     _write_assignment(config, "alias", "aliases", out, wrap='"')
